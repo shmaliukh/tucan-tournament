2e9f6b15be2e4eacdf4647a9d6f2e7a4583a084d
vshmaliukh <zskat02@gmail.com>
2023-06-30 08:08:54 +0300

    first commit

---
+++ src/main/java/xyz/vshmaliukh/tucantournament/TucanTournamentApplication.java
@@ -0,0 +1,13 @@
+package xyz.vshmaliukh.tucantournament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TucanTournamentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TucanTournamentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/vshmaliukh/tucantournament/TucanTournamentApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.vshmaliukh.tucantournament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TucanTournamentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
